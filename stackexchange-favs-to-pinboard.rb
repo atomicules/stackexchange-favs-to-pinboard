9ac3827315988dc97f860565e61a344860f92da3
atomicules <base@atomicules.co.uk>
2015-04-06 11:00:34 +0100

    Improvements to Pinboard code. Now a class, uses json and logger
    I used the Pinboard code from this script as a basis for some Jekyll
    syndication code I'm putting together elsewhere. I then made
    changes/improvements to it which I've decided to port back over here. I
    guess eventually I'll end up making my own Pinboard gem. Maybe.
    
    Some corresponding changes to the script were necessary such as command
    line arguments for Pinboard username and token, and re-ordering of method
    arguments.

--- stackexchange-favs-to-pinboard.rb
+++ stackexchange-favs-to-pinboard.rb
@@ -3,9 +3,12 @@ require 'open-uri'
 require 'json'
 require 'optparse'
 require 'cgi'
+require 'logger'
+
 
 optparse = OptionParser.new do |opts|
 	opts.on('-i', '--id ID', "Stackexchange ID") { |i| StackID = i }
+	opts.on('-u', '--user USER', "Pinboard username") { |u| User = u }
 	opts.on('-t', '--token TOKEN', "Pinboard API Token") { |t| Token = t }
 end
 optparse.parse!
@@ -26,38 +29,65 @@ def get_favs(site, id)
 	parsed = parse(response)
 end
 
-def pinboard_add(auth_token, url, description, replace, tags)
-	attempts = 1
-	posted = false
-	until ($rate_limit > 60) | (attempts > 3) | posted 
-		response = open(URI.encode("https://api.pinboard.in/v1/posts/add?auth_token=#{auth_token}&url=#{url}&description=#{description}&replace=#{replace}&tags=#{tags}").gsub("'", "%27"))
-		#Bit of a hacky kludge. For some reason URI.encode doesn't catch apostrophes.
-		if (response.status[0] == "200") & response.string.include?("done")
-			puts "Added #{url}"
-			posted = true
-		elsif (response.status[0] == "200") & response.string.include?("exists")
-			puts "Skipping #{url}, already exists"
-			posted = true
-		elsif response.status[0] == "429"
-			# 429 Too Many Requests, increase rate limit
-			$rate_limit *= 2
-			puts "Rate Limit increased to #{$rate_limit} seconds"
-		end
-		attempts += 1
-		#Rate limit as per Pinboard API requirements
-		sleep $rate_limit
+
+class Pinboard
+	@@logger = Logger.new(STDOUT)
+	@@logger.level = Logger::INFO
+	@@rate_limit = 3
+	Api_url = "https://api.pinboard.in/v1/"
+
+
+	def initialize(user, token)
+		@user = user
+		@token = token
 	end
-	if $rate_limit > 60
-		puts "Rate limit has exceeded 60 secs, let's try again another time"
-		quit
-	elsif attempts > 3
-		puts "Failed 3 times to save #{url}, bombing out"
-		quit
+
+
+	def add(url, description, extended=nil, tags=nil, replace="no")
+		attempts = 1
+		posted = false
+		#At minimum must have url and description
+		array_parameters = "&url=#{CGI.escape(url)}&description=#{CGI.escape(description)}"
+		#Could loop through the below
+		unless extended.nil?
+			array_parameters += "&extended=#{CGI.escape(extended)}"
+		end
+		unless tags.nil?
+			#TODO: Need to check whether tags_escaped will work
+			array_parameters += "&tags=#{CGI.escape(tags)}"
+		end
+		until (@@rate_limit > 60) | (attempts > 3) | posted 
+			response = open("#{Api_url}posts/add?auth_token=#{@user}:#{@token}"+array_parameters+"&replace=#{replace}&format=json")
+			@@logger.debug(response.string)
+			@@logger.debug(response.status)
+			response_json = JSON.parse(response.string)
+			if (response.status[0] == "200") & (response_json["result_code"] == "done")
+				@@logger.info("Added #{url}")
+				posted = true
+			elsif (response.status[0] == "200") & (response_json["result_code"] == "item already exists")
+				@@logger.info("Skipping #{url}, already exists")
+				posted = true
+			elsif response.status[0] == "429"
+				# 429 Too Many Requests, increase rate limit
+				@@rate_limit *= 2
+				@@logger.warn("Rate Limit increased to #{$rate_limit} seconds")
+			end
+			attempts += 1
+			#Rate limit as per Pinboard API requirements
+			sleep @@rate_limit
+		end
+		if @@rate_limit > 60
+			@@logger.error("Rate limit has exceeded 60 secs, let's try again another time")
+		elsif attempts > 3
+			@@logger.error("Failed 3 times to save #{url}, bombing out")
+		end
+		posted
 	end
 end
 
-if defined?(StackID) and defined?(Token)
-	$rate_limit = 3
+
+if defined?(StackID) and defined?(User) and defined?(Token)
+	pb = Pinboard.new(User, Token)
 	parsed = get_sites(StackID)
 	parsed["items"].each do |site|
 		favs = get_favs(site["site_url"].sub("http://", "").sub(".stackexchange", "").sub(".com", ""), site["user_id"])
@@ -65,8 +95,8 @@ if defined?(StackID) and defined?(Token)
 			title = fav["title"]
 			tags = fav["tags"]
 			link = fav["link"]
-			pinboard_add(Token, link, CGI.unescape_html(title), "no", tags.join(", ")+", stackexchangefavs")
-			#Need to unescape so can URI encode
+			#Need to unescape so can re-escape in Pinboard code
+			pb.add(link, CGI.unescape_html(title), nil, tags.join(", ")+", stackexchangefavs", "no")
 		end
 	end
 end
